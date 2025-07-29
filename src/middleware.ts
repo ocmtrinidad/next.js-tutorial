0855f685fb68456bb3f98468b2d2a769efeca4c1
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-29 12:30:59 +0800

    Worked with middleware

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// Two ways to create middleware:
+// 1. A function that returns a NextResponse with a matcher config
+// 2. Conditional statement that checks the request URL and returns a NextResponse.redirect() or rewrite().
+// redirect() changes the URL in the browser, while rewrite() keeps the URL but changes the content served.
+export function middleware(request: NextRequest) {
+  // return NextResponse.redirect(new URL("/", request.url));
+
+  // if (request.nextUrl.pathname === "/profile") {
+  //   return NextResponse.redirect(new URL("/", request.nextUrl));
+  // }
+
+  // Working with cookies and headers.
+  const response = NextResponse.next();
+  const themePreference = request.cookies.get("theme");
+  if (!themePreference) {
+    response.cookies.set("theme", "dark");
+  }
+  response.headers.set("custom-header", "custom-value");
+
+  return response;
+}
+
+// export const config = {
+//   matcher: "/profile",
+// };
