6f839e2a6dc60fa5ad1f389c851267d9395fa4fe
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-30 14:17:25 +0800

    Commented out import Slider

--- src/app/components/slider.tsx
+++ src/app/components/slider.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import React from "react";
-import Slider from "react-slick";
+// import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useTheme } from "./theme-provider";
 
 export default function ImageSlider() {
   const theme = useTheme();
-  const settings = {
-    dots: true,
-  };
+  // const settings = {
+  //   dots: true,
+  // };
   return <h1 style={{ color: theme.colors.primary }}>Slider</h1>;
 }
