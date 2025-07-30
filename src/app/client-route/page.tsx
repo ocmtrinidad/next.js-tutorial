3eca678fd06404394219be847b1913ea1191b7fb
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-30 11:52:18 +0800

    Deleted

--- src/app/client-route/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { serverFunction } from "@/utils/server-utils";
-
-export default function ClientRoutePage() {
-  const result = serverFunction();
-  return <h1>Client Route Page {result}</h1>;
-}
--- src/app/server-route/page.tsx
+++
@@ -1,6 +0,0 @@
-import { serverFunction } from "@/utils/server-utils";
-
-export default function ServerRoutePage() {
-  const result = serverFunction();
-  return <h1>Server Route Page {result}</h1>;
-}
