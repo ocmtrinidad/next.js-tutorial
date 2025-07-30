c0505790fc6402938e6cfd192b1cac25c89d33ac
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-30 14:09:15 +0800

    Updated

--- src/app/server-route/page.tsx
+++ src/app/server-route/page.tsx
@@ -1,12 +1,12 @@
-import { serverFunction } from "@/utils/server-utils";
+// import { serverFunction } from "@/utils/server-utils";
 import ImageSlider from "../components/slider";
 
 // Using server side (serverFunction) AND client side (ImageSlider) code.
 export default function ServerRoutePage() {
-  const result = serverFunction();
+  // const result = serverFunction();
   return (
     <>
-      <h1>Server route {result}</h1>
+      {/* <h1>Server route {result}</h1> */}
       <ImageSlider />
     </>
   );
