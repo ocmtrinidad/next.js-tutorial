80bc6ef0f08b7c5c54dbebaea310759bf457c730
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-30 10:16:30 +0800

    Worked with generateStaticParams() and dynamicParams

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,9 +1,27 @@
+// dynamicParams default to true. A true dynamicParams generates a html file in .next to serve if it isnt in generateStaticParams. A false dynamicParams returns a 404 error page.
+// export const dynamicParams = false;
+
+// generateStaticParams() is a Next.js feature that prerenders routes based on an array of params.
+export async function generateStaticParams() {
+  return [
+    {
+      productId: "1"
+    },
+    {
+      productId: "2"
+    },
+    {
+      productId: "3"
+    }
+  ]
+}
+
 const ProductDetails = async ({ params }: {
   params: Promise<{ productId: string }>;
 }) => {
   const { productId } = await params;
 
-  return <h1>Details about {productId}</h1>;
+  return <h1>Details about {productId} at {new Date().toLocaleTimeString()}</h1>;
 };
 
 export default ProductDetails;
