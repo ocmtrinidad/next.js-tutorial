2e12bcbdc20f600e8397928810bcd5e348a8abbe
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-30 11:28:42 +0800

    Update

--- src/app/product-reviews/page.tsx
+++ src/app/product-reviews/page.tsx
@@ -3,17 +3,17 @@ import Reviews from "../components/reviews";
 import { Suspense } from "react";
 
 // Streaming allows for UI to be rendered on a first come, first serve basis, improving performance.
-// <Suspense></Suspense> allows for elements to be instantly rendered while components are loading. 
+// <Suspense></Suspense> allows for elements to be instantly rendered while components are loading.
 export default function ProductReviews() {
   return (
     <div>
       <h1>Product Reviews</h1>
       <Suspense fallback={<p>Loading Product Details</p>}>
-        <Products/>
+        <Products />
       </Suspense>
       <Suspense fallback={<p>Loading Review Details</p>}>
-        <Reviews/>
+        <Reviews />
       </Suspense>
     </div>
-  )
+  );
 }
