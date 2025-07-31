459c951aa6ff0bdaab10c90effb139c8774b406c
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-31 10:34:17 +0800

    Example of client side fetching

---
+++ src/app/users-client/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+};
+
+export default function UsersClient() {
+  // Array of type User
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch users");
+        }
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        } else {
+          setError("An unknown error occured");
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchUsers();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
+
+  return (
+    <ul className="space-y-4 p-4">
+      {users.map((user) => (
+        <li
+          key={user.id}
+          className="p-4 bg-blue-500 shadow-md rounded-lg text-gray-700"
+        >
+          <div className="font-bold">{user.name}</div>
+          <div className="text-sm">
+            <div>Username: {user.username}</div>
+            <div>Email: {user.email}</div>
+            <div>Phone: {user.phone}</div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
