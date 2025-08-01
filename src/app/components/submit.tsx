664e9f090a754b9500d3c6251225e406b5b190ce
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-01 11:43:09 +0800

    Created submit component using useFormStatus()

---
+++ src/app/components/submit.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+
+export default function Submit() {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      disabled={pending}
+      className="block w-full p-2 bg-blue-500 rounded disabled:bg-gray-500"
+    >
+      Add Product
+    </button>
+  );
+}
