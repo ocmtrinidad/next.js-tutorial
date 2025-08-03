22ed01d7e59d5eb8fff6574b8210888a2abd1d87
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-03 08:21:28 +0800

    Created product edit form

---
+++ src/app/products-db/[id]/product-edit-form.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import Submit from "@/app/components/submit";
+import { FormState, createProduct } from "@/app/actions/products";
+import { useActionState } from "react";
+import { Product } from "../page";
+
+export default function ProductEditFormPage({ product }: { product: Product }) {
+  const initialState: FormState = {
+    errors: {},
+  };
+
+  // state is how you use the initialState (the errors that are returned by createProduct).
+  // formAction is how you call createProduct.
+  // isPending is similar to useFormStatus(), but useFormStatus() is specifically for forms, while isPending can be used for any action.
+  // Shows errors, but works.
+  const [state, formAction, isPending] = useActionState(
+    createProduct,
+    initialState
+  );
+
+  return (
+    <form action={formAction} className="p-4 space-y-4 max-w-96">
+      <div>
+        <label htmlFor="title">
+          Title
+          <input
+            type="text"
+            name="title"
+            id="title"
+            className="block w-full p-2 border rounded"
+            defaultValue={product.title}
+          />
+        </label>
+        {state.errors.title && (
+          <p className="text-red-500">{state.errors.title}</p>
+        )}
+      </div>
+
+      <div>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            name="price"
+            id="price"
+            className="block w-full p-2 border rounded"
+            defaultValue={product.price}
+          />
+        </label>
+        {state.errors.price && (
+          <p className="text-red-500">{state.errors.price}</p>
+        )}
+      </div>
+
+      <div>
+        <label htmlFor="description">
+          Description
+          <textarea
+            name="description"
+            id="description"
+            className="block w-full p-2 border rounded"
+            defaultValue={product.description ?? ""}
+          ></textarea>
+        </label>
+        {state.errors.description && (
+          <p className="text-red-500">{state.errors.description}</p>
+        )}
+      </div>
+
+      <Submit />
+    </form>
+  );
+}
