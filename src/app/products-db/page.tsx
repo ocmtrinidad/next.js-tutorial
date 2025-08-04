2fb004e17cc8cf04a65d1448c272b21b2510de27
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-04 10:03:15 +0800

    Importing <ProductsList/>

--- src/app/products-db/page.tsx
+++ src/app/products-db/page.tsx
@@ -1,6 +1,5 @@
 import { getProducts } from "@/prisma-db";
-import Link from "next/link";
-import { removeProduct } from "../actions/products";
+import ProductsList from "./products-list";
 
 export type Product = {
   id: number;
@@ -12,25 +11,5 @@ export type Product = {
 export default async function ProductsDBPage() {
   const products: Product[] = await getProducts();
 
-  return (
-    <ul className="space-y-4 p-4">
-      {products.map((product) => (
-        <li
-          key={product.id}
-          className="p-4 bg-black shadow-md rounded-lg text-white"
-        >
-          <h2 className="text-xl font-semibold">
-            <Link href={`/products-db/${product.id}`}>{product.title}</Link>
-          </h2>
-          <p>{product.description}</p>
-          <p className="text-lg font-medium">${product.price}</p>
-          <form action={removeProduct.bind(null, product.id)}>
-            <button className="px-4 py-2 mt-4 text-white bg-red-500 rounded-md hover:bg-red-800">
-              Delete
-            </button>
-          </form>
-        </li>
-      ))}
-    </ul>
-  );
+  return <ProductsList products={products} />;
 }
