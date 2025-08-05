36cbf8f942e267b132a3dc90e823339f031b232a
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-05 15:14:27 +0800

    Updated Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,26 @@
 // import styles from "./page.module.css";
-import Link from "next/link";
-import Search from "./components/search";
+// import Link from "next/link";
+// import Search from "./components/search";
 
-const Home = () => {
+// const Home = () => {
+//   return (
+//     <>
+//       <h1 className="text-red-600">Home</h1>
+//       <Search />
+//       <Link href={"/docs"} className="text-blue-500">
+//         Docs
+//       </Link>
+//       <Link href={"/products"}>Products</Link>
+//       <Link href={"/articles/breaking-news-123?lang=en"}>Read in English</Link>
+//       <Link href={"/articles/breaking-news-123?lang=fr"}>Read in French</Link>
+//     </>
+//   );
+// };
+
+export default function Home() {
   return (
-    <>
-      <h1 className="text-red-600">Home</h1>
-      <Search />
-      <Link href={"/docs"} className="text-blue-500">
-        Docs
-      </Link>
-      <Link href={"/products"}>Products</Link>
-      <Link href={"/articles/breaking-news-123?lang=en"}>Read in English</Link>
-      <Link href={"/articles/breaking-news-123?lang=fr"}>Read in French</Link>
-    </>
+    <div>
+      <h1>HOME PAGE NEXTAUTH</h1>
+    </div>
   );
-};
-
-export default Home;
+}
