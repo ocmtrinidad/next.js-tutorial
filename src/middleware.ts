9bc68fa6f84a5301b6ca17b6e2509a2ba1e858c0
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-06 11:07:56 +0800

    Middleware page protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,56 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 
-// Two ways to create middleware:
-// 1. A function that returns a NextResponse with a matcher config
-// 2. Conditional statement that checks the request URL and returns a NextResponse.redirect() or rewrite().
-// redirect() changes the URL in the browser, while rewrite() keeps the URL but changes the content served.
-export function middleware(request: NextRequest) {
-  // return NextResponse.redirect(new URL("/", request.url));
+// // Two ways to create middleware:
+// // 1. A function that returns a NextResponse with a matcher config
+// // 2. Conditional statement that checks the request URL and returns a NextResponse.redirect() or rewrite().
+// // redirect() changes the URL in the browser, while rewrite() keeps the URL but changes the content served.
+// export function middleware(request: NextRequest) {
+//   // return NextResponse.redirect(new URL("/", request.url));
 
-  // if (request.nextUrl.pathname === "/profile") {
-  //   return NextResponse.redirect(new URL("/", request.nextUrl));
-  // }
+//   // if (request.nextUrl.pathname === "/profile") {
+//   //   return NextResponse.redirect(new URL("/", request.nextUrl));
+//   // }
 
-  // Cookies and headers.
-  const response = NextResponse.next();
-  const themePreference = request.cookies.get("theme");
-  if (!themePreference) {
-    response.cookies.set("theme", "dark");
+//   // Cookies and headers.
+//   const response = NextResponse.next();
+//   const themePreference = request.cookies.get("theme");
+//   if (!themePreference) {
+//     response.cookies.set("theme", "dark");
+//   }
+//   response.headers.set("custom-header", "custom-value");
+
+//   return response;
+// }
+
+// // export const config = {
+// //   matcher: "/profile",
+// // };
+
+import withAuth from "next-auth/middleware";
+import { NextRequest, NextResponse } from "next/server";
+
+// Protecting pages with middleware.
+export default withAuth(
+  function middleware(request) {
+    if (
+      request.nextUrl.pathname.startsWith("/CreateUser") &&
+      request.nextauth?.token?.role != "admin"
+    ) {
+      // new URL("input", "base"). This creates a new URL by checking the base URL, then modifying the path based on the input. localhost:3000/CreateUser becomes localhost:3000/Denied.
+      return NextResponse.rewrite(new URL("/Denied", request.url));
+    }
+  },
+  {
+    // If user is authorized, give token
+    callbacks: {
+      // !! means NOT NULL
+      authorized: ({ token }) => !!token,
+    },
   }
-  response.headers.set("custom-header", "custom-value");
+);
 
-  return response;
-}
-
-// export const config = {
-//   matcher: "/profile",
-// };
+// Protects the /CreateUser route from users who are not logged in.
+export const config = {
+  matcher: ["/CreateUser"],
+};
