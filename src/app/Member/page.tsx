8e6b2ee686295f8a7fc257cb2ade8ad0fd10afd1
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-06 10:27:57 +0800

    Server side route protection

--- src/app/Member/page.tsx
+++ src/app/Member/page.tsx
@@ -1,7 +1,19 @@
-export default function Member() {
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { options } from "../api/auth/[...nextauth]/options";
+
+export default async function Member() {
+  const session = await getServerSession(options);
+
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/Member");
+  }
+
   return (
     <div>
       <h1>Member Server Session</h1>
+      <p>{session?.user?.email}</p>
+      <p>{session?.user?.role}</p>
     </div>
   );
 }
